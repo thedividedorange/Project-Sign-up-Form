eb06815d0e147c5bd4cd7cd8af6a9a8cab5f662a
thedividedorange <spawnpoint13@gmail.com>
2024-08-14 01:32:34 +0100

    update with themeswitch

---
+++ script.js
@@ -0,0 +1,11 @@
+window.addEventListener("load", function(){
+    const switchTheme = document.querySelector("#themeSwitch")
+    switchTheme.addEventListener("click", function(e){
+        document.documentElement.classList.toggle("ease")
+        document.documentElement.classList.toggle("dark")
+
+
+        switchTheme.classList.toggle("btn-warning")
+        switchTheme.classList.toggle("btn-danger")
+    })
+})
